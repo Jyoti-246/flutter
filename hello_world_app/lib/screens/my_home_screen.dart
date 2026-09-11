d4a84f8a223f03c5a85625d52e91b63c53aec7b0
Jyoti Kedia <jyotikedia203@gmail.com>
2026-09-11 13:45:17 +0530

    Update home screen

--- hello_world_app/lib/screens/my_home_screen.dart
+++ hello_world_app/lib/screens/my_home_screen.dart
@@ -7,14 +7,40 @@ class MyHomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello World App"),
         backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Text(
-          "Hello World",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text("Hello World App"), Text("Login Button")],
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 20),
+          Container(
+            height: 40,
+            width: 300,
+            color: Colors.green,
+            child: Center(
+              child: Text(
+                "Hello World",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            height: 40,
+            width: 300,
+            color: Colors.green,
+            child: Center(
+              child: Text(
+                "Hello World",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
